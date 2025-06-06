e0596a8bff46381ea456de6c4bc59d9fdebc1c07
Uriel Abraham <abraham2160@hotmail.com>
2025-06-06 14:42:11 -0700

    Recursividad
    Imprime una liste de números,  de manera recursiva, solicitando un inicio, final y un iterador

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,38 @@
-// ConsoleApplication1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+#include <string>
+#include <math.h>
 using namespace std;
+
+int imprimirRecursivo(int inicio, int final) {
+	if (final >= inicio) {
+		cout << inicio << "\n";
+		inicio ++;
+		imprimirRecursivo(inicio, final);
+	}
+	return 0;
+}
+
+int imprimirRecursivo(int inicio, int final, int cuantos) {
+	if (final >= inicio) {
+		cout << inicio << "\n";
+		inicio+=cuantos;
+		imprimirRecursivo(inicio, final, cuantos);
+	}
+	return 0;
+}
 
 int main()
 {
-    cout << "Hello World!\n";
+	int inicio = 0, final = 0, cuantos = 0;
+    cout << "Hello World!\n" << endl;
+	cout << "-------------------------------------------------------\t" << endl;
+	cout << "Cuantos numeros quiere imprimir?" << endl;
+	cin >> final;
+	cout << "\nDesde que numero quiere empezar a imprimir?" << endl;
+	cin >> inicio;
+	cout << "\nQuiere que se impriman de 1 en 1 o de n en n?\n"
+		 << "Tenga en cuenta que si va mas de 1 en 1, puede llegar al limite mas rapido" << endl;
+	cin >> cuantos;
+	cout << "\n" << endl;
+	imprimirRecursivo(inicio, final, cuantos);
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
